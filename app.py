40e5badf3545916c644e6b482267f0f09b6447c8
sebastyijan-fi <niklas@sebastyijan.fi>
2023-11-06 13:14:40 +0000

    update

--- app.py
+++ app.py
@@ -1,10 +1,76 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, jsonify, send_from_directory, request
+from random import choice, randint
+import openai
+from dotenv import load_dotenv
 
+# Load environment variables
+load_dotenv()
+
+# Initialize Flask app
 app = Flask(__name__)
+
+# OpenAI API key
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+# Directory for avatar images
+AVATAR_DIR = 'images'
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-if __name__ == "__main__":
+@app.route('/api/random-avatar')
+def random_avatar():
+    try:
+        # Get a list of avatars
+        avatars = os.listdir(os.path.join(app.static_folder, AVATAR_DIR))
+        avatars = [avatar for avatar in avatars if avatar.endswith('.png')]
+        # Pick a random avatar and send the filename
+        random_avatar = choice(avatars)
+        return jsonify({'avatar': random_avatar})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@app.route('/api/get-assignment')
+def get_assignment():
+    assignments = [
+        'Plan your next holiday',
+        'Organize a birthday party for a friend',
+        'Come up with a fitness routine',
+        # ... other assignments ...
+    ]
+    random_assignment = choice(assignments)
+    return jsonify({'assignment': random_assignment})
+
+@app.route('/api/interaction/<int:count>')
+def interaction(count):
+    # Assuming a max of 15 interactions
+    total_interactions = 15
+    interactions_left = max(total_interactions - count, 0)
+    return jsonify({'interactions_left': interactions_left})
+
+@app.route('/api/generate-message', methods=['POST'])
+def generate_message():
+    data = request.json
+    user_input = data.get('user_input')
+
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant. Reply with max 75 words. You should aanswer in a conversational manner to the user. So no points, lists..etc..Talk to me like a human being with emotions and realism."},
+                {"role": "user", "content": user_input}
+            ]
+        )
+        message = response.choices[0].message['content']
+        return jsonify({'message': message})
+    except openai.error.OpenAIError as e:
+        return jsonify({'error': str(e)}), 500
+
+@app.route('/static/<path:path>')
+def send_static(path):
+    return send_from_directory('static', path)
+
+if __name__ == '__main__':
     app.run(port=7666)
--- static/script.js
+++ static/script.js
@@ -1,64 +1,108 @@
-let mediaRecorder;
-let audioChunks = [];
-let isSpeaking = false;
-let audioResponse = new Audio();
+document.addEventListener('DOMContentLoaded', () => {
+    // Grabbing all the necessary DOM elements
+    const participateButton = document.getElementById('participate-button');
+    const mainHeader = document.getElementById('main-header');
+    const interactionCounter = document.getElementById('interaction-counter');
+    const avatarContainer = document.getElementById('avatar-container');
+    const aiResponse = document.getElementById('ai-response');
+    const userInteractionArea = document.getElementById('user-interaction-area');
+    const userInput = document.getElementById('user-input');
+    const submitButton = document.getElementById('submit-answer');
+    const questionnaire = document.getElementById('questionnaire');
+    let interactionCount = 15; // Initialize interaction count
 
-document.addEventListener('keydown', function(event) {
-    if (event.key === 'r') {
-        if (isSpeaking) {
-            stopSpeaking();
+    // Listen for click on the participate button to start the interaction
+    participateButton.addEventListener('click', startInteraction);
+
+    // Listen for click on the submit button to send input to the AI
+    submitButton.addEventListener('click', () => {
+        if (userInput.value.trim()) {
+            sendUserInputToAI(userInput.value);
+            userInput.value = ''; // Clear the input after sending
         } else {
-            startSpeaking();
+            alert('Please enter your answer.'); // Alert if input is empty
         }
+    });
+
+
+// Function to handle the start of interaction
+function startInteraction() {
+    // Hide the welcome header and show interaction elements
+    mainHeader.classList.add('hidden');
+    interactionCounter.classList.remove('hidden');
+    avatarContainer.classList.remove('hidden');
+    aiResponse.classList.remove('hidden');
+    userInteractionArea.classList.remove('hidden');
+    fetchRandomAvatar();
+    fetchRandomAssignment();
+}
+// Function to fetch a random avatar from the server
+function fetchRandomAvatar() {
+    // Send a GET request to the server to get a random avatar
+    fetch('/api/random-avatar')
+        .then(response => response.json())
+        .then(data => {
+            if (!data.error) {
+                // If no error, display the avatar
+                avatarContainer.innerHTML = `<img src="/static/images/${data.avatar}" alt="Avatar">`;
+            } else {
+                console.error('Error fetching avatar:', data.error);
+            }
+        })
+        .catch(error => console.error('Error fetching avatar:', error));
+}
+
+// Function to fetch a random assignment from the server
+function fetchRandomAssignment() {
+    // Send a GET request to the server to get a random assignment
+    fetch('/api/get-assignment')
+        .then(response => response.json())
+        .then(data => {
+            // Display the assignment in the AI response area
+            aiResponse.textContent = data.assignment;
+        })
+        .catch(error => console.error('Error fetching assignment:', error));
+}
+
+// Function to send the user's input to the AI and get a response
+function sendUserInputToAI(input) {
+    // Display a loading message or similar feedback in the AI response area
+    aiResponse.textContent = 'Thinking...';
+
+    // Send a POST request to the server with the user's input
+    fetch('/api/generate-message', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ user_input: input })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (!data.error) {
+            // If no error, display the AI's response
+            aiResponse.textContent = data.message;
+            updateInteractionCount();
+        } else {
+            aiResponse.textContent = 'An error occurred. Please try again.';
+            console.error('Error from AI:', data.error);
+        }
+    })
+    .catch(error => {
+        aiResponse.textContent = 'Failed to communicate with AI.';
+        console.error('Error interacting with AI:', error);
+    });
+}
+
+// Function to update the interaction count
+function updateInteractionCount() {
+    // Decrement the interaction count and update the display
+    interactionCount--;
+    const interactionCountElement = document.getElementById('interaction-count');
+    interactionCountElement.textContent = `${interactionCount}/15`;
+
+    // If all interactions are used, show the questionnaire
+    if (interactionCount <= 0) {
+        userInteractionArea.classList.add('hidden'); // Hide the user interaction area
+        questionnaire.classList.remove('hidden'); // Show the questionnaire
     }
+}
 });
-
-function startRecording() {
-    navigator.mediaDevices.getUserMedia({ audio: true })
-        .then(stream => {
-            mediaRecorder = new MediaRecorder(stream);
-            mediaRecorder.ondataavailable = event => {
-                audioChunks.push(event.data);
-            };
-            mediaRecorder.onstop = sendAudioToBackend;
-            mediaRecorder.start();
-        });
-}
-
-function stopRecording() {
-    mediaRecorder.stop();
-}
-
-function sendAudioToBackend() {
-    let audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
-
-    // Here, you'd typically send the audioBlob to your backend for processing
-    // and await the AI's response.
-    // Once you have the AI's audio response, play it for the user.
-    audioResponse.src = URL.createObjectURL(audioBlob); // Replace this with the AI's response URL
-    audioResponse.play();
-}
-
-audioResponse.addEventListener('ended', function() {
-    isSpeaking = false;
-});
-
-function startSpeaking() {
-    if (!isSpeaking) {
-        isSpeaking = true;
-        const aiEntity = document.getElementById('aiEntity');
-        aiEntity.style.animation = 'kittLights 0.5s infinite';
-        startRecording();
-    }
-}
-
-function stopSpeaking() {
-    if (isSpeaking) {
-        isSpeaking = false;
-        const aiEntity = document.getElementById('aiEntity');
-        aiEntity.style.animation = 'none';
-        stopRecording();
-        audioResponse.pause();
-        audioResponse.currentTime = 0;
-    }
-}
